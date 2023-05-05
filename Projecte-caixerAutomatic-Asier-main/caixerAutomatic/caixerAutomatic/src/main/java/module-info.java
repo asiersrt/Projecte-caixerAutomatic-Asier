ae29a763d882e75e355f3c45dceedf2e0e1adbf4
Asier <123738382+asiersrt@users.noreply.github.com>
2023-05-05 10:50:47 +0200

    Delete Projecte-caixerAutomatic-Asier-main/caixerAutomatic/caixerAutomatic/src/main/java directory

--- Projecte-caixerAutomatic-Asier-main/caixerAutomatic/caixerAutomatic/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.caixerautomatic {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.caixerautomatic to javafx.fxml;
-    exports com.mycompany.caixerautomatic;
-}
